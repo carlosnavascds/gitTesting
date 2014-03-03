ed9515952b4e5a1a5fc896248def8e72953179d5
Carlos Navas <cnavas@codigodelsur.com>
2014-03-03 11:55:46 -0200

    frag 1

--- src/com/example/gitTesting/Fragment1.java
+++ src/com/example/gitTesting/Fragment1.java
@@ -19,7 +19,8 @@ public class Fragment1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {ç
+    	// get view
        View v = inflater.inflate(R.layout.fragment1, null);
 
 
